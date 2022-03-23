c18f4832c0fb3be7a733176f48855b23254e386c
Jonatas Amaral <jonatas.amaral@jussi.com.br>
2022-03-22 22:53:52 -0300

    add multiplicacao via API externa

--- src/calc.js
+++ src/calc.js
@@ -7,3 +7,12 @@ export function add(...n) {
 export function sub(...n) {
 	return n.reduce((a, b) => a - b);
 }
+
+export async function webMultiply(...n) {
+	// send to https://api.mathjs.org/ and return result
+	return (await axios.get(`https://api.mathjs.org/v4/`, {
+		params: {
+			expr: n.join("*")
+		}
+	})).data;
+}
