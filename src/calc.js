5127068fc7b6eeea0b552eb66db6e65c758b9085
Jonatas Amaral <jonatas.amaral@jussi.com.br>
2022-03-23 23:00:39 -0300

    criar funcoes de multiplar

--- src/calc.js
+++ src/calc.js
@@ -1,5 +1,3 @@
-import axios from 'axios';
-
 export function add(...n) {
 	return n.reduce((a, b) => a + b);
 }
@@ -8,6 +6,24 @@ export function sub(...n) {
 	return n.reduce((a, b) => a - b);
 }
 
+export function multiply(...n) {
+	return n.reduce((a, b) => a * b);
+}
+
+export function adderMultiply(...n) {
+	let total = 0;
+	for (let i = 0; i < n.length - 1; i++) {
+		let set = total || n[i];
+		total = 0;
+		for (let j = 0; j < n[i + 1]; j++) {
+			total = add(total, set);
+		}
+	}
+
+	return total;
+}
+
+import axios from 'axios';
 export async function webMultiply(...n) {
 	// send to https://api.mathjs.org/ and return result
 	return (await axios.get(`https://api.mathjs.org/v4/`, {
